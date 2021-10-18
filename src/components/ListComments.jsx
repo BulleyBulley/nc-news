56545b90a039ba355365c21f819d284cf370b1bd
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-10-18 17:13:33 +0100

    adds comments for each article

--- src/components/ListComments.jsx
+++ src/components/ListComments.jsx
@@ -3,36 +3,29 @@ import { Link } from "react-router-dom";
 
 const ListComments = ({ comments }) => {
   return (
-    
-      <div className="comments_class">
-        <div className="comments_container">
-          <ul>
-            {comments.map((comment) => {
-              return (
-                <li key={comment.comment_id}>
-                  <div className="comments_list_container">
-                    <div className="comments_list_body">
-                    
-                      <h3>{comment.body}</h3>
-                      
-                    </div>
-                    <div className="comments_list_author">
-                    
-                      <h3>Author: {comment.author}</h3>
-                      
-                    </div>
-                    <div className="comments_list_vote">
-                      <h4>Votes: {comment.vote}</h4>
-                    </div>
-                    
+    <div className="comments_class">
+      <div className="comments_container">
+        <ul>
+          {comments.map((comment) => {
+            return (
+              <li key={comment.comment_id}>
+                <div className="comments_list_container">
+                  <div className="comments_list_body">
+                    <h3>{comment.body}</h3>
                   </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+                  <div className="comments_list_author">
+                    <h3>Author: {comment.author}</h3>
+                  </div>
+                  <div className="comments_list_vote">
+                    <h4>Votes: {comment.vote}</h4>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-    
+    </div>
   );
 };
 
