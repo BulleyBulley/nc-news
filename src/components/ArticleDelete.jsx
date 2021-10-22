3fd9513ce02dd74cf39c33936c6ebf6bf0eab9d2
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-10-22 23:41:54 +0100

    adds delete article

--- src/components/ArticleDelete.jsx
+++ src/components/ArticleDelete.jsx
@@ -1,42 +1,34 @@
 import { useState, useContext } from "react";
-import { deleteArticle } from "../utils/Api"
+import { deleteArticle } from "../utils/Api";
 import { RequiresLogin, UserContext } from "../utils/User";
 import { goBack } from "../utils/Util";
 
-const ArticleDelete = ({article_id}) => {
-    const [error, setIsError] = useState(false)
-    const { isLoggedIn } = useContext(UserContext)
+const ArticleDelete = ({ article_id }) => {
+  const [error, setIsError] = useState(false);
+  const { isLoggedIn } = useContext(UserContext);
 
-    const handleDelete = () => {
+  const handleDelete = () => {
+    deleteArticle(article_id).then(() => {
+      goBack();
+    });
+  };
+  return (
+    <>
+      <RequiresLogin isLoggedIn={isLoggedIn}>
+        <div className="article_delete_button_container">
+          <label htmlFor="article_delete_button_id">Delete</label>
 
-        deleteArticle(article_id).then(() => {
-          goBack()
-        })
-        
-    }
-    return (
-        <>
-        <RequiresLogin isLoggedIn={isLoggedIn}>
-          <div className="article_delete_button_container">
-            <label htmlFor="article_delete_button_id">
-              Delete
-            </label>
-            
-            <button
-              className="article_delete_button"
-              id="article_delete_button_id"
-              onClick={handleDelete}
-            >
-              <i className="fa fa-trash" aria-hidden="true"></i>
+          <button
+            className="article_delete_button"
+            id="article_delete_button_id"
+            onClick={handleDelete}
+          >
+            <i className="fa fa-trash" aria-hidden="true"></i>
+          </button>
+        </div>
+      </RequiresLogin>
+    </>
+  );
+};
 
-            </button>
-    
-            </div>
-            </RequiresLogin>
-          
-        </>
-      );
-
-}
-
-export default ArticleDelete
+export default ArticleDelete;
